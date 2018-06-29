5599dc4b853be8ef935e087196b4f0604004145e
jiangxf <jiangxf@ctrip.com>
2018-06-29 12:07:45 +0800

    update

---
+++ ng2-select2-ex/ng2-select2-ex.component.js
@@ -0,0 +1,12 @@
+class Ng2Select2ExController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: Ng2Select2ExController,
+    template: require('./ng2-select2-ex.html'),
+    bindings: {
+        
+    }
+};
